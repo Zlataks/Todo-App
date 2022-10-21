0ce58e086223b255dceb6677817667f248896273
Kseniia Briaschei <briaschei@gmail.com>
2022-10-21 13:18:30 +0300

    create a container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Button from '@mui/material/Button';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import Container from '@mui/material/Container';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="contained">Hello World</Button>
+      <GlobalStyles
+        styles={{
+          body: { backgroundColor: '#FFD700', border: '1px solid #000000' }
+        }}
+      />
+      <Container sx={{ backgroundColor: '#FFFFFF', width: '1032px', height: '688px', 
+        boxShadow: '0px 8px 24px rgba(0, 13, 48, 0.12)',  margin: '86px auto'}} />
     </div>
   );
 }
