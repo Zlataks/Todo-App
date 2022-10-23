30ba19111812df7d927a0c74cc05d3a0b13dfbfb
Kseniia Briaschei <briaschei@gmail.com>
2022-10-23 20:43:33 +0300

    fix container

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import GlobalStyles from '@mui/material/GlobalStyles';
 import Container from '@mui/material/Container';
 
-function App() {
-  return (
-    <div className="App">
-      <GlobalStyles
-        styles={{
-          body: { backgroundColor: '#FFD700', border: '1px solid #000000' }
-        }}
-      />
-      <Container sx={{ backgroundColor: '#FFFFFF', width: '1032px', height: '688px', 
-        boxShadow: '0px 8px 24px rgba(0, 13, 48, 0.12)',  margin: '86px auto'}} />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div style={{backgroundColor: '#FFD700', border: '1px solid #000000'}} className="App">
+        <Container sx={{ backgroundColor: '#FFFFFF', width: '1032px', height: '688px', 
+          boxShadow: '0px 8px 24px rgba(0, 13, 48, 0.12)',  margin: '86px auto'}} />
+      </div>
+    );
+  }
 }
 
 export default App;
