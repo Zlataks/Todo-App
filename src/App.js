fac4991a21d2b74ee134e16e9507c31129439f1a
Kseniia Briaschei <briaschei@gmail.com>
2022-10-24 15:52:47 +0300

    change container`s design

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Container from '@mui/material/Container';
 class App extends React.Component {
   render() {
     return (
-      <div style={{backgroundColor: '#FFD700', border: '1px solid #000000'}} className="App">
+      <div style={{backgroundColor: '#FFD700'}} className="App">
         <Container sx={{ backgroundColor: '#FFFFFF', width: '1032px', height: '688px', 
           boxShadow: '0px 8px 24px rgba(0, 13, 48, 0.12)',  margin: '86px auto'}} />
       </div>
