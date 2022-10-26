fe87fda22e66f6aad5fe98f2eefd110af8c6b88e
Kseniia Briaschei <briaschei@gmail.com>
2022-10-26 17:45:06 +0300

    fix layout bag, extract components

---
+++ src/components/TaskItem.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import {Checkbox, Box, Typography, IconButton} from '@mui/material';
+import {KeyboardArrowDown, KeyboardArrowUp, EditOutlined, DeleteOutlineOutlined} from '@mui/icons-material';;
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
+class TaskItem extends React.Component {
+    render() {
+      return (
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: 465,
+            height: 24,
+            margin: 'auto',
+            paddingTop: '13px'
+        }}>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-start',
+              alignItems: 'center',
+            }}>
+              <Checkbox {...label} defaultChecked
+                sx={{
+                  padding: '10px',
+                  '&.Mui-checked': {
+                    color: '#00B573'
+                  },
+                }} 
+              />
+              <Typography variant="body1" gutterBottom sx={{
+                  height: 15,
+                  fontFamily: 'Helvetica Neue',
+                  fontSize: 16,
+                }} >
+                Publish Website
+              </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              height: 23,
+              width: 166,
+              opacity: 0,
+              ':hover': {
+                opacity: 1,
+              }
+            }}>
+            <IconButton>
+              <KeyboardArrowDown />
+            </IconButton>
+            <IconButton>
+              <KeyboardArrowUp />
+            </IconButton>
+            <IconButton>
+              <EditOutlined />
+            </IconButton>
+            <IconButton sx={{
+              color: '#EB476A'
+            }}>
+              <DeleteOutlineOutlined />
+            </IconButton>
+          </Box>
+        </Box>
+      );
+    }
+}
+
+export default TaskItem;
