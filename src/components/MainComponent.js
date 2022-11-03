fb67e31997c2d64a2241969c134c6f17fc1eef29
Kseniia Briaschei <briaschei@gmail.com>
2022-11-04 00:13:33 +0300

    change mock data

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,19 +8,46 @@ import Input from './Input';
 class MainComponent extends React.Component {
 
     render() {
-        const tasks = ['Create mock date', 'Show list of tasks', 'Change style for first and last items'];
+
+        const date = {
+            "tasks":[
+               {
+                  "status":"ACTIVE",
+                  "priority":true,
+                  "order":54,
+                  "name":"Create mock date",
+                  "id":"6360035bbeefc90012c2e096"
+               },
+               {
+                "status":"ACTIVE",
+                "priority":true,
+                "order":54,
+                "name":"Show list of tasks",
+                "id":"7360035bbeefc90012c2e096"
+               },
+               {
+                "status":"ACTIVE",
+                "priority":true,
+                "order":54,
+                "name":"Change style for first and last items",
+                "id":"8360035bbeefc90012c2e096"
+               }
+            ]
+        };
 
         const components = [];
 
-        tasks.forEach((item, i, arr) => {
+        let tasks = date.tasks;
+
+        for (let i = 0; i < tasks.length; i++) {
             if (i === 0) {
-                components.push(<TaskItem first={true} text={item}/>)
-            } else if (i === arr.length - 1) {
-                components.push(<TaskItem last={true} text={item}/>)
+                components.push(<TaskItem first={true} text={tasks[i].name}/>)
+            } else if (i === tasks.length - 1) {
+                components.push(<TaskItem last={true} text={tasks[i].name}/>)
             } else {
-                components.push(<TaskItem text={item}/>)
+                components.push(<TaskItem text={tasks[i].name}/>)
             }
-        })
+        }
 
         return (
             <div className="Wrapper">
