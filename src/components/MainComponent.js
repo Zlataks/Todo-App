41f9b41de61848fa28de2f0dd2007df0fda5e9c8
Kseniia Briaschei <briaschei@gmail.com>
2022-11-15 14:40:24 +0300

    use get request to retriew the list of task, and view the list

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,47 +1,65 @@
 import React from 'react';
-import {Container} from '@mui/material';
+import {Container, Typography} from '@mui/material';
 import TaskItem from './TaskItem';
 import Input from './Input';
 
 
 
 class MainComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+           tasks: [],
+           errorMessage: null
+        };
+     }
+
+    componentDidMount() {
+        fetch('https://todo-be-production.up.railway.app/tasks')
+           .then((response) => response.json())
+           .then(
+            (result) => {
+                if (result.status !== 500) {
+                    this.setState({
+                        tasks: result.tasks
+                      });
+                } else {
+                    this.setState({ errorMessage: result.message });
+                }
+              },
+           )
+           .catch(error => {
+            this.setState({ errorMessage: error.message });
+           });
+    }
 
     render() {
-
-        const data = {
-            "tasks":[
-               {
-                  "status":"NOTACTIVE",
-                  "priority":true,
-                  "order":54,
-                  "name":"Create mock date",
-                  "id":"6360035bbeefc90012c2e096"
-               },
-               {
-                "status":"ACTIVE",
-                "priority":true,
-                "order":54,
-                "name":"Show list of tasks",
-                "id":"7360035bbeefc90012c2e096"
-               },
-               {
-                "status":"ACTIVE",
-                "priority":true,
-                "order":54,
-                "name":"Change style for first and last items",
-                "id":"8360035bbeefc90012c2e096"
-               }
-            ]
-        };
-
         const components = [];
-        data.tasks.forEach((item, i, arr) => {
-            components.push(<TaskItem first={i === 0} last={i === arr.length - 1} text={item.name} checked={item.status !== "ACTIVE"}/>)
-          })
-
+        if (this.state.errorMessage) {
+            components.push(<Typography variant="body1" gutterBottom sx={{
+                fontSize: 16,
+                fontFamily: 'Helvetica Neue',
+                textAlign: 'center'
+              }}>
+              Something went wrong. Please try later
+          </Typography>)
+        } else if (this.state.tasks) {
+            this.state.tasks.forEach((item, i, arr) => {
+                components.push(<TaskItem first={i === 0} last={i === arr.length - 1} text={item.name} checked={item.status !== "ACTIVE"}/>)
+            })
+        } else if (!this.state.tasks){
+            components.push(<Typography variant="body1" gutterBottom sx={{
+                fontSize: 16,
+                fontFamily: 'Helvetica Neue',
+                textAlign: 'center'
+              }}>
+              No task yet
+          </Typography>)
+        }
+        
         return (
             <div className="Wrapper">
+                <div></div>
                 <Container sx={{ backgroundColor: '#FFFFFF', width: '1032px', height: '688px', 
                     boxShadow: '0px 8px 24px rgba(0, 13, 48, 0.12)',  margin: 'auto'}}>
                     <Input />
